e155a813fc6549f5c880edca227c4ca2e26aeaac
DanielFair <fair3620@mylaurier.ca>
2017-01-11 01:25:39 -0800

    new port

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+var port = process.env.PORT || 3000;
+
 app.get('/', (req, res) => {
     res.end('Timestamp API Project 0.0.1');
-}).listen(8080);
+}).listen(port, () => {
+    console.log('Timestamp app listening on port '+port+'!');
+});
