317e61d9fff9a8ec410959450e941ddb186fe01a
DanielFair <fair3620@mylaurier.ca>
2017-01-11 23:40:07 -0800

    added initial html

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var moment = require('moment');
+var path = require('path');
 var app = express();
 var port = process.env.PORT || 3000;
 
@@ -7,11 +8,13 @@ var monthArr = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 
 var regEx = /[a-z]/gi;
 
 function isDate(date) {
-    // return ( (new Date(date) !== "Invalid Date" && !isNaN(new Date(date)) ));
     return ((new Date(date).toString() !== "Invalid Date" && !isNaN(new Date(date)) ));
 }
+
+
 app.get('/', (req, res) => {
-    res.end('Welcome to the Timestamp API Microservice! To use the API, enter a unix timestamp or natural language date (January 1, 2017) as a paramter in the URL.');
+    res.sendFile(path.join(__dirname+'/index.html'));
+    //res.end('Welcome to the Timestamp API Microservice! To use the API, enter a unix timestamp or natural language date (January 1, 2017) as a parameter in the URL.');
 })
 app.get('/:dateparam', (req, res) => {
     var dateParam = req.params.dateparam;
@@ -24,7 +27,7 @@ app.get('/:dateparam', (req, res) => {
         if(isDate(dateParam)) {
             var naturalDate = new Date(dateParam);
             console.log('and next');
-            dateObj.unix = (naturalDate.getTime())/1000;
+            dateObj.unix = Math.round((naturalDate.getTime())/1000);
             dateStr = monthArr[naturalDate.getMonth()] + ' ' + naturalDate.getDate() + ', ' + naturalDate.getFullYear();
             dateObj.natural = dateStr;
         }
@@ -42,7 +45,10 @@ app.get('/:dateparam', (req, res) => {
         dateObj.natural = dateStr;
         console.log(dateObj);
     }
-
+    if(dateObj.unix.length == 0) {
+        dateObj.unix = null;
+        dateObj.natural = null;
+    }
     res.end(JSON.stringify(dateObj));
 }).listen(port, () => {
     console.log('Timestamp app listening on port '+port+'!');
